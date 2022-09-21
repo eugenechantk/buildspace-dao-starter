fb1c5a1b60d4200fbacfc1c624f38a73081d3d7c
Eugene Chan <me@eugenechantk.me>
2022-09-21 00:15:26 -0300

    Added revoke role script

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const token = sdk.getToken("0xe42E6F7DA9081C37ef0BaBfA66d99e093EDbf8d7");
+
+(async () => {
+  try {
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
